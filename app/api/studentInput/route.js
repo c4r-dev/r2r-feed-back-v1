cc5dfabf951b8c0db52bb1738fde45299be9aea5
Prathima <prse@seas.upenn.edu>
2024-10-02 14:30:14 -0400

    adding the name

--- app/api/studentInput/route.js
+++ app/api/studentInput/route.js
@@ -3,8 +3,8 @@ import StudentInput from "@/models/studentFeedbackWidget";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { answerQ1, fbtool } = await request.json();
+  const { answerQ1, fbtool, activityName } = await request.json();
   await connectMongoDB();
-  await StudentInput.create({ answerQ1, fbtool, activityName  });
+  await StudentInput.create({ answerQ1, fbtool, activityName });
   return NextResponse.json({ message: "Feedback Submitted" }, { status: 201 });
 }
